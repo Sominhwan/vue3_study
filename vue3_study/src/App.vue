895956ad9bd62f70dc9823eb68e5d4223f465f7c
Sominhwan <thalsghks@naver.com>
2023-12-25 19:21:44 +0900

    20231225 SMH [vue3] 설치 및 study

---
+++ vue3_study/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div>{{ name }}</div>
+    <div>{{ name2 }}</div>
+    <button class="btn btn-primary" @click="updateName()">Click1</button>
+    <button class="btn btn-primary" @click="updateName2()">Click2</button>
+  </div>
+</template>
+
+<script setup>
+  import { reactive, ref } from 'vue';
+
+  const name = ref('Hello World!');
+  const name2= reactive({
+    id: 1
+  });
+  // const greeting = (name) => {
+  //   return 'Hello, ' + name;
+  // };
+
+  const updateName = () => {
+   name.value = 'Hello World2'
+   console.log(name);
+  }
+  const updateName2 = () => {
+   name2.id = 2;
+   console.log(name2);
+  }
+</script>
+<style scoped>
+  .name {
+    color: red;
+  }
+</style>
